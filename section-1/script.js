3f3ff6747920512234ee3dce3b14d149e6773186
jakubwrobel2003 <83750224+jakubwrobel2003@users.noreply.github.com>
2022-08-16 23:20:40 +0200

    10

--- section-1/script.js
+++ section-1/script.js
@@ -1,4 +1,8 @@
 let js = "amazing";
-if (js === "amazing") alert("js is fun");
 
 console.log(40 + 8 + 2 - 100);
+console.log("kkuyba");
+console.log(23);
+let firstName = "kuba";
+console.log(firstName);
+let PI = 3.1415;
