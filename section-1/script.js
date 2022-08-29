5b6c940ba712e28ca9ad8a32ae6d302fefe30675
jakubwrobel2003 <83750224+jakubwrobel2003@users.noreply.github.com>
2022-08-29 19:58:10 +0200

    12

--- section-1/script.js
+++ section-1/script.js
@@ -1,8 +1,42 @@
-let js = "amazing";
+/*let js = "amazing";
 
 console.log(40 + 8 + 2 - 100);
 console.log("kkuyba");
 console.log(23);
 let firstName = "kuba";
 console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+
+let kubaWrobel = 'kw';
+let $function =27;
+let person ="kuba";
 let PI = 3.1415;
+
+let myFirstJob ="Coder";
+let myCurrentJob ="teacher"
+ let job1 = "programer"
+ let job2 ="teacher"
+
+ console.log(
+  myFirstJob
+ )
+*/
+true;
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof "jonas");
+javaScriptIsFun = "yes!";
+console.log(typeof javaScriptIsFun);
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1991;
+console.log(year);
+console.log(typeof year);
+console.log(typeof null);
