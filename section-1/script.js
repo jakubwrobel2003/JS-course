fdd4441a0cdee9a8734a629a2c5d6896f3aa9bf2
jakubwrobel2003 <83750224+jakubwrobel2003@users.noreply.github.com>
2022-09-04 00:10:44 +0200

    22

--- section-1/script.js
+++ section-1/script.js
@@ -125,3 +125,61 @@ let myCurrentJob ="teacher"
 // ////////////////////////////////////
 // //Challange #2
 // ////////////////////////////////////
+// const marksMass = 78;
+// const marksHight = 1.69;
+// const johnMass = 92;
+// const johnHight = 1.95;
+
+// const marksBMI = marksMass / (marksHight * marksHight);
+// console.log(marksBMI);
+// const johnsBMI = johnMass / (johnHight * johnHight);
+// console.log(johnsBMI);
+// let markHugherBMI = marksBMI > johnsBMI;
+// console.log(markHugherBMI);
+
+// if (marksBMI > johnsBMI) {
+//   console.log(
+//     `Mark's BMI (${marksBMI}) is higher than John's (${johnsBMI}) BMI`
+//   );
+// } else {
+//   console.log(`John (${johnsBMI}) BMI is higher than Mark's BMI (${marksBMI})`);
+// }
+// ////////////////////////////////////
+
+//type  conversion
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(inputYear + 1);
+// console.log(Number("Jonas"));
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+// //type coercing
+// console.log(`I'm ` + 23 + ` years old`);
+// console.log("23" - "10" - 3);
+// console.log("23" / "2");
+// let n = "1" + 1; //11
+// n -= 1;
+// console.log(n);
+
+////////////////////////
+//5 falsy values: 0, '', undefined, null, NaN
+////////////////////////
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+const money = 0;
+if (money) {
+  console.log("Don't spend it all;)");
+} else {
+  console.log("You should get a job");
+}
+let hight;
+if (hight) {
+  console.log("Hight is defined");
+} else {
+  console.log("Hight is UNDEFINED");
+}
