9383e30090f7caae31675299b95c794010b439e2
jakubwrobel2003 <83750224+jakubwrobel2003@users.noreply.github.com>
2022-09-08 22:58:20 +0200

    123

--- section-1/script.js
+++ section-1/script.js
@@ -166,20 +166,40 @@ let myCurrentJob ="teacher"
 ////////////////////////
 //5 falsy values: 0, '', undefined, null, NaN
 ////////////////////////
-console.log(Boolean(0));
-console.log(Boolean(undefined));
-console.log(Boolean("Jonas"));
-console.log(Boolean({}));
-console.log(Boolean(""));
-const money = 0;
-if (money) {
-  console.log("Don't spend it all;)");
-} else {
-  console.log("You should get a job");
-}
-let hight;
-if (hight) {
-  console.log("Hight is defined");
-} else {
-  console.log("Hight is UNDEFINED");
-}
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("Jonas"));
+// console.log(Boolean({}));
+// console.log(Boolean(""));
+// const money = 0;
+// if (money) {
+//   console.log("Don't spend it all;)");
+// } else {
+//   console.log("You should get a job");
+// }
+// let hight;
+// if (hight) {
+//   console.log("Hight is defined");
+// } else {
+//   console.log("Hight is UNDEFINED");
+// }
+////////////////////////////////////////////////
+////////////////////////
+// const age = "18";
+// if (age === 18) console.log("jesteś dorosly strict");
+// if (age == 18) console.log("jesteś dorosly loose");
+// const fav = Number(prompt("what is your favorite nubmer"));
+// console.log(fav);
+// console.log(typeof fav);
+// if (fav === 23) {
+//   console.log("cool! 23 is an amaizng number");
+// } else if (fav === 7) {
+//   console.log("7 is ok");
+// } else {
+//   console.log("number is not 23 or 7");
+// }
+// if (fav !== 23) console.log("why no 23");
+////////////////////////////////////////////////
+////////////////////////
+const hasDriversLicense = true;
+const hasGoodVisoin = true;
