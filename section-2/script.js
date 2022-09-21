03c4a0d12d37d7eafe0aed0c4f5e01b15a83b4de
jakubwrobel2003 <83750224+jakubwrobel2003@users.noreply.github.com>
2022-09-21 20:11:15 +0200

    33

--- section-2/script.js
+++ section-2/script.js
@@ -6,3 +6,23 @@
 // if (hasDriversLicense) console.log("I cant driv :D");
 // // const interface = "Audio";
 // // const privete = 534;
+
+function looger() {
+  console.log("My name is Kuba");
+}
+looger();
+looger();
+looger();
+looger();
+
+function fruitProcessor(apples, ogranges) {
+  // console.log(apples, ogranges);
+  const juice = `Juice with ${apples} apples and ${ogranges} ogranges`;
+  return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
