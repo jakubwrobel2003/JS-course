5cba261566b11bd1b7c18aff31930dedada88d7d
jakubwrobel2003 <83750224+jakubwrobel2003@users.noreply.github.com>
2022-12-28 22:32:43 +0100

    97

--- section-6/script.js
+++ section-6/script.js
@@ -31,37 +31,51 @@
 // // nie działa console.log(age);
 // // nie diała printAge();
 // // nie maa dostepu do danych
+//////
+/////
+/////
+// console.log(me);
+// //console.log(job);
+// //console.log(year);
+// var me = `Jonas`;
+// let job = 'teacher';
+// const year = 1991;
 
-console.log(me);
-//console.log(job);
-//console.log(year);
-var me = `Jonas`;
-let job = 'teacher';
-const year = 1991;
+// // function
+// console.log(addDelc(2, 3));
+// //console.log(addExpr(2, 3));
+// //console.log(addArrow(2, 3));
+// function addDelc(a, b) {
+//   return a + b;
+// }
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+// var addArrow = (a, b) => a + b;
 
-// function
-console.log(addDelc(2, 3));
-//console.log(addExpr(2, 3));
-//console.log(addArrow(2, 3));
-function addDelc(a, b) {
-  return a + b;
-}
-const addExpr = function (a, b) {
-  return a + b;
+// //example
+// console.log(undefined);
+// if (!numProducts) delteShopingCart();
+// var numProducts = 10;
+// function delteShopingCart() {
+//   console.log(`All poducts deleted!`);
+// }
+
+// var x = 1;
+// let y = 2;
+// const z = 3;
+// console.log(x === window.x);
+// console.log(y === window.y);
+// console.log(z === window.z);
+
+console.log(this);
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
 };
-var addArrow = (a, b) => a + b;
-
-//example
-console.log(undefined);
-if (!numProducts) delteShopingCart();
-var numProducts = 10;
-function delteShopingCart() {
-  console.log(`All poducts deleted!`);
-}
-
-var x = 1;
-let y = 2;
-const z = 3;
-console.log(x === window.x);
-console.log(y === window.y);
-console.log(z === window.z);
+calcAge(1991);
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+calcAgeArrow(1980);
