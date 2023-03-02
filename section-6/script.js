3709cab55589b657803fa720995a3cc7296a7a36
jakubwrobel2003 <83750224+jakubwrobel2003@users.noreply.github.com>
2023-03-02 08:43:54 +0100

    a

--- section-6/script.js
+++ section-6/script.js
@@ -140,3 +140,53 @@
 //   return a + b;
 // };
 // addArrow(2, 4, 5);
+
+// let age = 30;
+// let oldAge = age;
+// age = 31;
+// console.log(age);
+// console.log(oldAge);
+
+// const me = {
+//   name: 'Jonas',
+//   age: 30,
+// };
+// const friend = me;
+// friend.age = 27;
+// console.log('Friend:', friend);
+// console.log('Me:', me);
+
+//primitive types
+let lastName = `Williams`;
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+//Reference types
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Willinams',
+  age: 27,
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage:', marriedJessica);
+
+// marriedJessica = {};
+
+//Copying objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Willinams',
+  age: 27,
+  family: ['Alice, Bob'],
+};
+const jessicaCopy = Object.assign({}, jessica2); //łaczy objekty shadow coppy
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
