34431bcde867c715b6efbbb92f08b093d9b41ab4
jakubwrobel2003 <83750224+jakubwrobel2003@users.noreply.github.com>
2023-07-25 13:15:44 +0200

    656565

--- section-9/script.js
+++ section-9/script.js
@@ -42,6 +42,33 @@ const restaurant = {
   },
 };
 
+//chalange
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+const events = new Set([...gameEvents.values()]);
+gameEvents.delete(64);
+console.log(gameEvents);
+console.log(events);
+const gameEventsSize = gameEvents.size;
+console.log(
+  `An event happened, on average, every ${90 / gameEventsSize} minutes`
+);
+for (const [key, value] of gameEvents) {
+  if (key <= 45) console.log(`[FIRST HALF] ${key}: ${value}`);
+  else console.log(`[SECOND HALF] ${key}: ${value}`);
+}
 // const question = new Map([
 //   ['question', 'What is the best programing langueage in the world?'],
 //   [1, 'C'],
