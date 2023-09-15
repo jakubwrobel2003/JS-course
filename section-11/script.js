3c728bc9e1d1b8d1473c1f84ed7e66fcbaa39dc7
jakubwrobel2003 <83750224+jakubwrobel2003@users.noreply.github.com>
2023-09-15 14:01:28 +0200

    array

--- section-11/script.js
+++ section-11/script.js
@@ -61,9 +61,11 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-const displayMovments = function (movements) {
+const displayMovments = function (movements, sort = false) {
   containerMovements.innerHTML = ` `;
-  movements.forEach(function (mov, i) {
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? `deposit` : `withdrawal`;
     const html = ` 
     <div class="movements__row">
@@ -193,6 +195,11 @@ btnClose.addEventListener('click', function (e) {
     containerApp.style.opacity = 0;
   }
   inputCloseUsername.value = inputClosePin.value = '';
+});
+let sorted = false;
+btnSort.addEventListener(`click`, function (e) {
+  e.preventDefault();
+  displayMovments(currentAccount.movements, !sorted);
 });
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
@@ -354,14 +361,70 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // console.log(accounts);
 // const account = accounts.find(acc => acc.owner === `Jessica Davis`);
 // console.log(account);
-console.log(movements);
-console.log(movements.includes(-130));
-const anyDeposits = movements.some(mov => mov > 1500);
-console.log(anyDeposits);
+// console.log(movements);
+// console.log(movements.includes(-130));
+// const anyDeposits = movements.some(mov => mov > 1500);
+// console.log(anyDeposits);
 
-console.log(movements.every(mov => mov > 0));
+// console.log(movements.every(mov => mov > 0));
 
-const deposit = mov => mov > 0;
-console.log(movements.some(deposit));
-console.log(movements.every(deposit));
-console.log(movements.filter(deposit));
+// const deposit = mov => mov > 0;
+// console.log(movements.some(deposit));
+// console.log(movements.every(deposit));
+// console.log(movements.filter(deposit));
+// const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+// console.log(arr.flat());
+// const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 9];
+// console.log(arrDeep.flat(2));
+
+// const overalBalance = accounts
+//   .flatMap(acc => acc.movements)
+
+// //   .reduce((acc, mov) => acc + mov, 0);
+// // console.log(overalBalance);
+// const owners = [`Jonas`, `Zach`, `Adam`, `Martha`];
+// console.log(owners.sort());
+// console.log(owners);
+// console.log(movements);
+// //return<0,A,B keep order
+// //retunr>0,B,A switch order
+// // movements.sort((a, b) => {
+// //   if (a > b) return 1;
+// //   if (a < b) return -1;
+// // });
+// // console.log(movements);
+// // movements.sort((a, b) => {
+// //   if (a > b) return -1;
+// //   if (a < b) return 1;
+// // });
+
+// movements.sort((a, b) => a - b);
+// console.log(movements);
+// movements.sort((a, b) => b - a);
+// console.log(movements);
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+//empty arr +fill method
+const x = new Array(7);
+console.log(x);
+console.log(x.map(() => 5));
+x.fill(1, 3, 5);
+x.fill(1);
+console.log(x);
+arr.fill(23, 4, 6);
+console.log(arr);
+
+// array from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+labelBalance.addEventListener(`click`, function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll(`.movements__value`),
+    el => el.textContent.replace(`€`, '')
+  );
+  console.log(movementsUI);
+  const movementsUI2 = [...document.querySelectorAll(`.movements__value`)];
+});
