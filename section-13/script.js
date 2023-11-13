be79053f11358db8777bbd18e881f3095148da9d
jakubwrobel2003 <83750224+jakubwrobel2003@users.noreply.github.com>
2023-11-13 21:53:13 +0100

    op

--- section-13/script.js
+++ section-13/script.js
@@ -166,6 +166,25 @@ const headerObserwer = new IntersectionObserver(stickyNav, {
 headerObserwer.observe(header);
 ////////////////////
 ////////////////////
+//revile section
+const allSection = document.querySelectorAll(`.section`);
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove(`section--hidden`);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.05,
+});
+allSection.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add(`section--hidden`);
+});
 ///////////////
 // console.log(document.documentElement);
 
